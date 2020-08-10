12ae96d39260524cc5e96901d0b12d6aaf518efd
Nazar Kalinichenko <nazarchik1701@gmail.com>
2020-08-10 21:55:11 +0300

    added rudimentary parsing and ShowHandler v2

---
+++ src/handlers/ShowHandler.java
@@ -0,0 +1,69 @@
+package handlers;
+
+import com.sun.net.httpserver.HttpExchange;
+import models.LoggedEntity;
+import utils.Logger;
+
+import java.io.*;
+
+public class ShowHandler extends AbstractHandler implements Logger{
+
+    public ShowHandler(boolean useArtifactLogging) {
+        super(useArtifactLogging);
+    }
+
+    @Override
+    public String handleGet(HttpExchange exchange) {
+        try {
+            BufferedReader bf = new BufferedReader(new FileReader(new File(logsPath)));
+            StringBuilder result = new StringBuilder();
+            String str = "";
+            while((str = bf.readLine()) != null)
+                result.append(str);
+            return gitresult.toString();
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+        return "FAIL";
+    }
+
+
+    @Override
+    public String handleDelete(HttpExchange exchange) {
+        return "Not yet implemented";
+    }
+
+    @Override
+    public String handleUpdate(HttpExchange exchange) {
+        return "Not yet implemented";
+    }
+
+    @Override
+    public String handlePost(HttpExchange exchange) {
+        return "Not yet implemented";
+    }
+
+    @Override
+    public void sendResponse(HttpExchange exchange, Object param) throws IOException {
+        OutputStream response = exchange.getResponseBody();
+        StringBuilder responseBody = new StringBuilder();
+        responseBody.append("<h1>").append("ShowHandler").append("</h1>").
+                append("<p>").append(param.toString()).append("</p>");
+
+        exchange.sendResponseHeaders(200, responseBody.toString().length());
+        response.write(responseBody.toString().getBytes());
+        response.flush();
+        response.close();
+    }
+
+    @Override
+    public void deposit(Object obj) {
+
+    }
+
+    @Override
+    public Object retrieve(int ind) {
+        return null;
+    }
+}
